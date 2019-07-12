b71fd5bf5a0176c70f29fb834d286591252e7ecf
Benjamin Makowsky <benjamin.makowsky@leidos.com>
2019-07-12 07:16:40 -0700

    Success on moving back and forth for forage

--- xpilot_ai/c/bee.c
+++ xpilot_ai/c/bee.c
@@ -171,7 +171,7 @@ void forage() {
 
 
   static int fuelLVL = 0;
-  //Act on POI
+  
   if(!inVicinityOf(x,y)) {
     refuel(0);
     sprintf(bugstring, "Forage: Moving to location (%d, %d) ",x,y);
