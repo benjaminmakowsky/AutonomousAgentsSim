eb1421318cc5a894573305be829f50cef7f01909
makowskyb <benjamin.makowsky@leidos.com>
2019-08-17 12:40:58 -0700

    Began working on language system

---
+++ xpilot_ai/c/bee_lang.c
@@ -0,0 +1,5 @@
+//
+// Created by makowskyb on 8/17/19.
+//
+
+#include "bee_lang.h"
---
+++ xpilot_ai/c/bee_lang.h
@@ -0,0 +1,8 @@
+//
+// Created by makowskyb on 8/17/19.
+//
+
+#ifndef XPILOT_LE_BEE_LANG_H
+#define XPILOT_LE_BEE_LANG_H
+
+#endif //XPILOT_LE_BEE_LANG_H
