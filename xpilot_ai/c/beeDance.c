250931549d69c741f2bbe779067ec68f9bfa0a8a
makowskyb <benjamin.makowsky@leidos.com>
2019-08-17 18:53:10 -0700

    Switched bee_lang to beeDance started consolidating all dance functions here

--- xpilot_ai/c/beeDance.c
+++ xpilot_ai/c/beeDance.c
@@ -2,20 +2,23 @@
 // Created by makowskyb on 8/17/19.
 //
 
-#include "bee_lang.h"
+#include "beeDance.h"
 #include "beeObject.h"
 
-
+//Local Variables
+static char danceMoves[4] = {none,none,none,none}; //Array to hold dance moves
 int msgType[2] = {foundSource, foundEnemy};
 
 
+int danceTree[10] = {0,1,2,3,4,5,6,7,8,9};
+
+//Helper Methods
+int getParent(int child){
+    return danceTree[child/2 -1];
+}
 
 
-
-
-
-
-int* buildMsg(int msgType){
+char* buildDance(int msgType){
     int* msgPointer;
     int msg[3]; //0: type 1: x coord 2: y_coord
 
@@ -23,7 +26,6 @@ int* buildMsg(int msgType){
     {
         case foundSource:
             msg[0] = msgType;
-            msg[1] =
     }
 
 }
--- xpilot_ai/c/beeDance.h
+++ xpilot_ai/c/beeDance.h
@@ -2,13 +2,15 @@
 // Created by makowskyb on 8/17/19.
 //
 
-#ifndef XPILOT_LE_BEE_LANG_H
-#define XPILOT_LE_BEE_LANG_H
+#ifndef XPILOT_LE_BEE_DANCE_H
+#define XPILOT_LE_BEE_DANCE_H
 
 //Defined values
 #define foundSource 0
 #define foundEnemy 1
-
+#define left 'l'
+#define right 'r'
+#define none '\0'
 
 //Array to hold message types ie (0: found honey, 1: found enemy hive) etc
 //Using an int so that when the message is transmitted its all in ints for the coordinates
@@ -17,8 +19,8 @@ extern int msgType[2];
 /// Builds the message for the dance
 /// \param msgType
 /// \return
-int* buildMsg(int msgType);
+char* buildDance(int msgType);
 
 
 
-#endif //XPILOT_LE_BEE_LANG_H
+#endif //XPILOT_LE_BEE_DANCE_H
